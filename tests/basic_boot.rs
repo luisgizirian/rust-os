1152214f79b7c9aac94bb4aa1ddce10ef888b1e5
luisgizirian <luigi101@gmail.com>
2022-07-06 09:59:14 -0500

    Complete Post 4 (Unit & Integration testing)

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -9,29 +9,17 @@ use blog_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello Rust {}", "OS!");
-    
-    #[cfg(test)]
     test_main();
 
-    loop{}
-}
-
-// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
     loop {}
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info);
 }
 
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
