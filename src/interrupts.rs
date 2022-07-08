fba94b3db402074c2a91228a376aa7a8efc011a6
luisgizirian <luigi101@gmail.com>
2022-07-08 10:28:37 -0500

    Post 7 (complete) - Hardware Interrupts

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -21,23 +21,76 @@ lazy_static! {
         }
         idt[InterruptIndex::Timer.as_usize()]
             .set_handler_fn(timer_interrupt_handler);
+        idt[InterruptIndex::Keyboard.as_usize()]
+            .set_handler_fn(keyboard_interrupt_handler);
 
         idt
     };
 }
 
+pub fn init_idt() {
+    IDT.load();
+}
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(
+    _stack_frame: InterruptStackFrame)
+{
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(layouts::Us104Key, ScancodeSet1,
+                HandleControl::Ignore)
+            );
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    // let key = match scancode {
+    //     0x02 => Some('1'),
+    //     0x03 => Some('2'),
+    //     0x04 => Some('3'),
+    //     0x05 => Some('4'),
+    //     0x06 => Some('5'),
+    //     0x07 => Some('6'),
+    //     0x08 => Some('7'),
+    //     0x09 => Some('8'),
+    //     0x0a => Some('9'),
+    //     0x0b => Some('0'),
+    //     _ => None,
+    // };
+    // if let Some(key) = key {
+    //     print!("{}", key);
+    // }
+
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
+    }
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
+    }
+}
+
 extern "x86-interrupt" fn timer_interrupt_handler(
-    _stack_frame: InterruptStackFrame) {
+    _stack_frame: InterruptStackFrame)
+{
     print!(".");
 
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
     }
-}
-
-pub fn init_idt() {
-    IDT.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame)
@@ -54,6 +107,7 @@ extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame,
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC_1_OFFSET,
+    Keyboard,
 }
 
 impl InterruptIndex {
